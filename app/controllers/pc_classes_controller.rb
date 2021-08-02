e160484b177a8020b40beed5793c95a59ac2db83
Donald Smith <donald@IIF-Dev.local>
2021-08-02 15:07:30 -0400

    working but ugly

--- app/controllers/pc_classes_controller.rb
+++ app/controllers/pc_classes_controller.rb
@@ -1,4 +1,9 @@
 class PcClassesController < ApplicationController
   def index
+    @pc_classes = PcClass.all.map { |x| x.name }
+    @submitted_value1 = params[:name1]
+    @submitted_value2 = params[:name2]
+    @first_class = PcClass.find_by name: params[:name1]
+    @second_class = PcClass.find_by name: params[:name2]
   end
 end
