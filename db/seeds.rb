5a98b5051159fad8cba2adc02e90250615d5c7e0
Donald Smith <donald@IIF-Dev.local>
2021-08-02 13:17:05 -0400

    Clean up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,79 +6,79 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Pc_class.create(name: 'Assassin',
+PcClass.create(name: 'Assassin',
                 l1: 0,      l2: 1751,      l3: 3501,      l4: 7001,
                 l5: 14001,  l6: 25001,     l7: 50001,     l8: 90001,
                 l9: 150001, l10: 200001,   l11: 350001,   l12: 500001,
                 hd: 'd6')
 
-Pc_class.create(name: 'Barbarian',
+PcClass.create(name: 'Barbarian',
                 l1: 0,      l2: 2101,      l3: 4701,      l4: 9401,
                 l5: 20001,  l6: 40001,     l7: 80001,     l8: 170001,
                 l9: 340001, l10: 600001,   l11: 800001,   l12: 1000001,
                 hd: 'd12')
 
-Pc_class.create(name: 'Bard',
+PcClass.create(name: 'Bard',
                 l1: 0,      l2: 1501,      l3: 3251,      l4: 7501,
                 l5: 15001,  l6: 30001,     l7: 60001,     l8: 120001,
                 l9: 240001, l10: 450001,   l11: 625001,   l12: 800001,
                 hd: 'd10')
 
-Pc_class.create(name: 'Cleric',
+PcClass.create(name: 'Cleric',
                 l1: 0,      l2: 2251,      l3: 5001,      l4: 9001,
                 l5: 18001,  l6: 35001,     l7: 70001,     l8: 140001,
                 l9: 300001, l10: 425001,   l11: 650001,   l12: 900001,
                 hd: 'd8')
 
-Pc_class.create(name: 'Druid',
+PcClass.create(name: 'Druid',
                 l1: 0,      l2: 2001,      l3: 4251,      l4: 8501,
                 l5: 17001,  l6: 35001,     l7: 70001,     l8: 180001,
                 l9: 275001, l10: 400001,   l11: 525001,   l12: 650001,
                 hd: 'd8')
 
-Pc_class.create(name: 'Fighter',
+PcClass.create(name: 'Fighter',
                 l1: 0,      l2: 2001,      l3: 4001,      l4: 8501,
                 l5: 17001,  l6: 34001,     l7: 68001,     l8: 136001,
                 l9: 272001, l10: 500001,   l11: 750001,   l12: 1000001,
                 hd: 'd10')
 
-Pc_class.create(name: 'Illusionist',
+PcClass.create(name: 'Illusionist',
                 l1: 0,      l2: 2601,      l3: 5201,      l4: 10401,
                 l5: 20801,  l6: 42501,     l7: 85001,     l8: 170001,
                 l9: 340001, l10: 500001,   l11: 750001,   l12: 900001,
                 hd: 'd4')
 
-Pc_class.create(name: 'Knight',
+PcClass.create(name: 'Knight',
                 l1: 0,      l2: 2251,      l3: 4501,      l4: 9001,
                 l5: 18001,  l6: 36001,     l7: 72001,     l8: 150001,
                 l9: 300001, l10: 600001,   l11: 725001,   l12: 900001,
                 hd: 'd10')
 
-Pc_class.create(name: 'Monk',
+PcClass.create(name: 'Monk',
                 l1: 0,      l2: 1751,      l3: 4001,      l4: 8501,
                 l5: 20001,  l6: 40001,     l7: 80001,     l8: 160001,
                 l9: 325001, l10: 550001,   l11: 750001,   l12: 1250001,
                 hd: 'd12')
 
-Pc_class.create(name: 'Paladin',
+PcClass.create(name: 'Paladin',
                 l1: 0,      l2: 2701,      l3: 5501,      l4: 12001,
                 l5: 24001,  l6: 48001,     l7: 95001,     l8: 180001,
                 l9: 360001, l10: 700001,   l11: 1000001,  l12: 1300001,
                 hd: 'd10')
 
-Pc_class.create(name: 'Ranger'
+PcClass.create(name: 'Ranger',
                 l1: 0,      l2: 2251,      l3: 4501,      l4: 9001,
                 l5: 18001,  l6: 40001,     l7: 75001,     l8: 150001,
                 l9: 250001, l10: 500001,   l11: 725001,   l12: 950001,
                 hd: 'd10')
 
-Pc_class.create(name: 'Rogue',
+PcClass.create(name: 'Rogue',
                 l1: 0,      l2: 1251,      l3: 2501,      l4: 6001,
                 l5: 12001,  l6: 24001,     l7: 48001,     l8: 80001,
                 l9: 120001, l10: 175001,   l11: 325001,   l12: 450001,
                 hd: 'd6')
 
-Pc_class.create(name: 'Wizard',
+PcClass.create(name: 'Wizard',
                 l1: 0,      l2: 2601,      l3: 5201,      l4: 10401,
                 l5: 20801,  l6: 42501,     l7: 85001,     l8: 170001,
                 l9: 340001, l10: 500001,   l11: 750001,   l12: 1000001,
