aa596b1b525025e6024f46396398073e2546e2b5
Donald Smith <donald@IIF-Dev.local>
2021-08-03 09:41:09 -0400

    fix bug with initial load

--- app/controllers/pc_classes_controller.rb
+++ app/controllers/pc_classes_controller.rb
@@ -3,7 +3,12 @@ class PcClassesController < ApplicationController
     @pc_classes = PcClass.all.map { |x| x.name }
     @submitted_value1 = params[:name1]
     @submitted_value2 = params[:name2]
-    @first_class = PcClass.find_by name: params[:name1]
-    @second_class = PcClass.find_by name: params[:name2]
+    if params.key?[:name1]
+      @first_class = PcClass.find_by name: params[:name1]
+      @second_class = PcClass.find_by name: params[:name2]
+    else
+      @first_class = PcClass.find_by name: 'Assassin'
+      @second_class = PcClass.find_by name: 'Assassin'
+    end
   end
 end
