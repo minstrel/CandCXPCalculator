8c5bddac300e00d91e8ef601699d52f54963d8ae
Donald Smith <donald@IIF-Dev.local>
2021-08-03 09:45:36 -0400

    typo

--- app/controllers/pc_classes_controller.rb
+++ app/controllers/pc_classes_controller.rb
@@ -3,7 +3,7 @@ class PcClassesController < ApplicationController
     @pc_classes = PcClass.all.map { |x| x.name }
     @submitted_value1 = params[:name1]
     @submitted_value2 = params[:name2]
-    if params.key?[:name1]
+    if params.key?(:name1)
       @first_class = PcClass.find_by name: params[:name1]
       @second_class = PcClass.find_by name: params[:name2]
     else
